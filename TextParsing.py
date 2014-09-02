151fc585503a0cbdc47fdbf2d142a319ad693af6
tuannguyenthe <tuan.nguyenthe@gmail.com>
2014-09-03 00:10:59 +0700

    can read messages. next task is putting to db

--- TextParsing.py
+++ TextParsing.py
@@ -1,97 +1,64 @@
 import struct
 import datetime
 import os
-from pymongo import *
+import message
 
 
 def toInt(byte):
-    return int.from_bytes(byte, byteorder='big',signed=False)
+    return int.from_bytes(byte, byteorder='big', signed=False)
 
-def readHeader(input):
-    if(len(input) != 12):
+#just ignore header record
+def readheader(input):
+    if len(input) != 12:
         return -1
     else:
         lastUpdate = [toInt(input[0:4]), toInt(input[4:8])]
         lastSequence = toInt(input[8:12])
         return [lastUpdate, lastSequence]
 
-def readRecord(input):
-    if(len(input) !=24):
+
+def readrecord(input):
+    if len(input) != 24:
         return -1
     else:
-        timeStamp = [toInt(input[0:4]), toInt(input[4:8])]
-        sequence = toInt(input[8:12])
-        opcode = toInt(input[12:16])
-        position = toInt(input[16:20])
-        contSize = toInt(input[20:22])
-        return [timeStamp, sequence, opcode, position, contSize]
+        return [toInt(input[0:4]), toInt(input[8:12]), toInt(input[12:16]), toInt(input[16:20]), toInt(input[20:22])]
 
-def parseIdx(direction, path, linkID):
+def parseLog(direction, path, linkID):
     fileIdx = direction + 'cont' + str(linkID) + '.idx'
-    fileIdxsize = os.path.getsize(path + fileIdx)
-    fileIdxHandler = open(path + fileIdx, "rb")
-    #print('Filesize is: {0} and Valid: {1} '.format(fileIdxsize, (fileIdxsize - 12)%24==0))
-    readHeader(fileIdxHandler.read(12))
-    output = []
-    rec=0
-    while rec!= -1:
-        rec = readRecord(fileIdxHandler.read(24))
-        if rec != -1:
-            output.append(rec)
-            #print(rec)
-    fileIdxHandler.close()
-    return output
+    fileLog = direction + 'cont' + str(linkID) + '.log'
 
-def parseLog(posts, direction, path, linkID, idx):
-    fileLog = direction + 'cont'+ str(linkID) + '.log'
-    fileLogsize = os.path.getsize(path + fileLog)
-    fileLogHandler = open(path + fileLog, "r", encoding="latin1")
-    #print('Filesize is: {0}'.format(fileLogsize))
-    output = []
-    while idx != []:
-        idxSequence = idx.pop()
-        fileLogHandler.seek(idxSequence[3],0)
-        currentSequence = fileLogHandler.read(idxSequence[4])
-        str1 = ''
-        post = {}
-        
-        #handle inbound messages
-        while direction =='i' and (currentSequence.find('1I') != -1 or currentSequence.find('1C') != -1):
-            if currentSequence.find('1I') > -1:
-                str1 = currentSequence[0:currentSequence.find('1I')]
-                post = readInbound('1I',currentSequence[currentSequence.find('1I'): currentSequence.find('1I')+70] )
-                currentSequence = currentSequence[0:currentSequence.find('1I')] + currentSequence[currentSequence.find('1I') + 70: -1]
-            if currentSequence.find('1C') > -1:
-                str1 = currentSequence[0:currentSequence.find('1C')]
-                readInbound('1C', currentSequence[currentSequence.find('1C'): currentSequence.find('1C')+17] )
-                currentSequence = currentSequence[0:currentSequence.find('1C')] + currentSequence[currentSequence.find('1C') + 17: -1]
-            #print(str1)
-        
-        #handle outbound messages
-        while direction == 'o' and (currentSequence.find('2C') != -1 or currentSequence.find('2E') != -1 or currentSequence.find('2G') != -1):
-            if currentSequence.find('2C') > -1:
-                str1 = currentSequence[0:currentSequence.find('2C')]
-                readOutbound('1I',currentSequence[currentSequence.find('2C'): currentSequence.find('2C')+26] )
-                currentSequence = currentSequence[0:currentSequence.find('2C')] + currentSequence[currentSequence.find('1I') + 26: -1]
-            if currentSequence.find('2E') > -1:
-                str1 = currentSequence[0:currentSequence.find('2E')]
-                readOutbound('2E', currentSequence[currentSequence.find('2E'): currentSequence.find('2E')+40] )
-                currentSequence = currentSequence[0:currentSequence.find('2E')] + currentSequence[currentSequence.find('2E') + 40: -1]
-            if currentSequence.find('2G') > -1:
-                str1 = currentSequence[0:currentSequence.find('2G')]
-                readOutbound('2G', currentSequence[currentSequence.find('2G'): currentSequence.find('2G')+ 240] )
-                currentSequence = currentSequence[0:currentSequence.find('2G')] + currentSequence[currentSequence.find('2G') + 240: -1]
-            #print(str1)
-        
-        if idxSequence[1] != 0 and post !=-1:
-            datetime_temp = datetime.datetime.fromtimestamp(idxSequence[0][0])
-            #datetime_temp.milisecond = idxSequence[0][1]
-            post['datetime'] = datetime_temp
-            post['sequenceNO'] = idxSequence[1]
-            posts.insert(post)
-    fileLogHandler.close()
-    print(output)    
-    return output
+    with open(path + fileIdx, "rb") as fileIdxHandler, open(path + fileLog, "rb") as fileLogHandler:
+        #just ignore the readheader function
+        readheader(fileIdxHandler.read(12))
+        rec = 0
+        while rec != -1:
+            rec = readrecord(fileIdxHandler.read(24))
+            if rec == -1:
+                break
+            fileLogHandler.seek(rec[3], 0)
+            currentsequence = fileLogHandler.read(rec[4])
+            if currentsequence.decode(errors='replace').find('ARP') != -1:
+                continue
+
+
+            #removing front padding
+            currentsequence = currentsequence[14:]
+
+
+            #read messages
+            while len(currentsequence) > 1:
+                msg = currentsequence[:message.messagelength.get(currentsequence[0:2].decode())].decode()
+                print(msg)  # this is what we need put this to database
+                currentsequence = currentsequence[1 + message.messagelength.get(currentsequence[0:2].decode()):]
+
+            #timestamp is here
+            #if idxSequence[1] != 0 and post !=-1:
+            #    datetime_temp = datetime.datetime.fromtimestamp(idxSequence[0])
+            #    #datetime_temp.milisecond = idxSequence[0][1]
+            #    post['datetime'] = datetime_temp
+            #    post['sequenceNO'] = idxSequence[1]
+
+    return 0
 
 def readInbound(controlledInput, input):
     # Read new order message 1I
@@ -157,7 +124,7 @@ def readOutbound(controlledInput, input):
         orderCancelStatus = input[25:26]
         return [message, firm, cancelShares, orderNO, orderEntryDate,orderCancelStatus]
     else:
-            return -1 # should raise exception rather than -1
+            return -1  # should raise exception rather than -1
 
 def read2G(input):
     return 0
@@ -165,16 +132,7 @@ def read2G(input):
 def read2E(input):
     return 0
 
-
-client = MongoClient('localhost', 27017)
-db = client['Message-Database']
-collection = db['Message-Collection']
-posts = db.posts
-
-path = "E:/HOSE/Ho Tro Phan Mem/Check2G/"
-linkID = 2
-idx1 = parseIdx('i', path, linkID)
-parseLog(posts, 'i', path, linkID, idx1 )
-#idx2 = parseIdx('o', path, linkID)
-#parseLog('o', path, linkID, idx2 )
-
+path = "D:/Logs/15-08/"
+linkID = 1
+#idx1 = parseIdx(path, linkID)
+parseLog('i', path, linkID)
--- message.py
+++ message.py
@@ -19,3 +19,7 @@ def parse2G(input):
     originalMessage = input[7:240]
     return [message, firm, reasonCode, originalMessage]
 
+# A bunch of constant for message types
+messagelength = {'1C':17, '1D':44, '1E':66, '1F':191, '1G': 120, '1I':70, '2B':17, '2C': 26, '2D':50,
+                 '2E':40, '2F':52, '2G': 240, '2I':49, '2L': 40, '3A':82, '3B': 95, '3C':27, '3D': 12,
+                 'RN': 484, 'RP':484, 'RQ': 11, 'AA': 52, 'BR': 53}
